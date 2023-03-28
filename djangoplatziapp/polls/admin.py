c2b6cac48ca579940f1b1295ed34eda84c197572
aldopuga01 <56364832+aldopuga01@users.noreply.github.com>
2023-03-28 15:25:44 -0600

    Creación de template para index

--- djangoplatziapp/polls/admin.py
+++ djangoplatziapp/polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    can_delete = False
+    verbose_name_plural = 'Choices'
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
--- djangoplatziapp/polls/urls.py
+++ djangoplatziapp/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Example: /polls/
     path('', views.index, name='index'),
+    # Example: /polls/1/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # Example: /polls/1/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Example: /polls/1/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
